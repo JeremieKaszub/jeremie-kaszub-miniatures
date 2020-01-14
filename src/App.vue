3cbd8ad04c3b219109ce35d00ba04a587e63015b
Jeremie Kaszub <jeremie.kaszub@gmail.com>
2020-01-14 14:13:34 +0100

    feat(template): add two columns template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <TwoColumns>
+      <template v-slot:aside> <Menu /> </template>
+      <template v-slot:main> <router-view /></template>
+    </TwoColumns>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import TwoColumns from "@/components/ui/TwoColumns.vue";
 
-#nav {
-  padding: 30px;
+import Menu from "@/components/navigation/Menu.vue";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    TwoColumns,
+    Menu
   }
-}
+};
+</script>
+
+<style lang="scss">
 </style>
---
+++ src/components/ui/TwoColumns.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="ui-two-columns">
+    <aside class="ui-two-columns__aside">
+      <slot name="aside" />
+    </aside>
+    <main class="ui-two-columns__main">
+      <slot name="main" />
+    </main>
+  </div>
+</template>
