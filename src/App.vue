2b3705c3ec89f658eb43f065ca35800bd6fc1525
Jeremie Kaszub <jeremie.kaszub@gmail.com>
2020-01-15 17:32:55 +0100

    feat(header): design header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <TwoColumns>
       <template v-slot:aside> <Menu /> </template>
       <template v-slot:main> <router-view /></template>
@@ -8,12 +9,13 @@
 </template>
 
 <script>
+import Header from "@/components/global/Header.vue";
 import TwoColumns from "@/components/ui/TwoColumns.vue";
-
 import Menu from "@/components/navigation/Menu.vue";
 
 export default {
   components: {
+    Header,
     TwoColumns,
     Menu
   }
